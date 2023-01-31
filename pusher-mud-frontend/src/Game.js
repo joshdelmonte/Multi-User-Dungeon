42060f51277fc28095a1d4ef7c5000b2ab0713e8
Reed Dahltorp <rdahltorp@gmail.com>
2023-01-30 21:09:29 -0700

    Adjusted commands in package json and begain work on CSS.

--- pusher-mud-frontend/src/Game.js
+++ pusher-mud-frontend/src/Game.js
@@ -17,7 +17,7 @@ class Game extends Component {
 
     render() {
         return (
-            <div className="row">
+            <div className="row tempBackgroundColor1">
                 <div className="col-8">
                     <div className="game-roomDescription">
                         Room Description Here
