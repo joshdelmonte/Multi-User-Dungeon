683c5fca40f81c1de1be1e7b47431037dac2c6cb
DoengesBA <110855912+DoengesBA@users.noreply.github.com>
2023-01-30 20:20:41 -0700

    Adding some world elements

--- pusher-mud-frontend/src/pusher.js
+++ pusher-mud-frontend/src/pusher.js
@@ -1,7 +1,7 @@
 import Pusher from 'pusher-js';
 
-const socket = new Pusher('PUSHER_KEY', {
-    cluster: 'PUSHER_CLUSTER',
+const socket = new Pusher('f95940f0b4976a427f4a', {
+    cluster: 'us3',
     encrypted: true,
     authEndpoint: 'http://localhost:4000/pusher/auth'
 });
