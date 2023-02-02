c79aee3cc7cdaa92c0950e8990c76040c5293088
DoengesBA <110855912+DoengesBA@users.noreply.github.com>
2023-02-01 21:57:24 -0700

    Resolve Bugs w/ World + Add Messaging

--- pusher-mud-backend/routes/graphql.js
+++ pusher-mud-backend/routes/graphql.js
@@ -1,32 +1,31 @@
 const users = require('../users');
 const graphqlTools = require('graphql-tools');
 const worldData = require('../world.json');
+const pusher = require('./pusher').pusher;
 
 const GraphqlSchema = `
-type Query {
-    version: String
-}
-type Mutation {
-    signin(sessionId: ID!, name: String!, race: String!, class: String!): String
-}
 type Door {
     name: ID!
     state: String!
 }
+
 type Exit {
     name: ID!
     description: String!
     door: Door!
 }
+
 type Room {
     name: ID!
     description: String!
     exits: [Exit!]!
 }
+
 type Query {
     version: String
     room(name: ID!) : Room
 }
+
 type ExitRoomError {
     reason: ID!
 }
@@ -38,8 +37,25 @@ type Mutation {
     openDoor(room: ID!, exit: ID!): Door!
     closeDoor(room: ID!, exit: ID!): Door!
     exitRoom(room: ID!, exit: ID!): ExitRoomResult!
+    speak(room: ID!, sessionId: ID!, message: String!): String
+    shout(room: ID!, sessionId: ID!, message: String!): String
+}
 `;
 
+function broadcastRoomUpdatesForDoor(doorName) {
+    const targetRoomName = Object.keys(worldData.rooms)
+        .filter(roomName => {
+            const roomDetails = worldData.rooms[roomName];
+            const hasDoor = Object.keys(roomDetails.exits)
+                .map(exitName => roomDetails.exits[exitName])
+                .map(exitDetails => exitDetails.door)
+                .includes(doorName);
+            return hasDoor;
+        })
+        .forEach(roomName => {
+            pusher.trigger('presence-room-' + roomName, 'updated', {});
+        });
+}
 
 const resolvers = {
     Query: {
@@ -51,6 +67,7 @@ const resolvers = {
                 const exits = Object.keys(roomExits)
                     .map((exitName) => {
                         const exit = roomExits[exitName];
+
                         return {
                             name: exitName,
                             description: exit.name,
@@ -80,15 +97,19 @@ const resolvers = {
         signin: (_, user) => {
             users.registerUser(user.sessionId, user.name, user.race, user.class);
             return "Success";
-        }
+        },
         openDoor: (_, { room, exit }) => {
             const roomData = worldData.rooms[room];
             const exitData = roomData.exits[exit];
             const doorName = exitData.door;
             const doorData = worldData.doors[doorName];
+
             if (doorData.state === 'closed') {
                 doorData.state = 'open';
             }
+
+            broadcastRoomUpdatesForDoor(doorName);
+
             return {
                 name: doorName,
                 state: doorData.state
@@ -99,9 +120,13 @@ const resolvers = {
             const exitData = roomData.exits[exit];
             const doorName = exitData.door;
             const doorData = worldData.doors[doorName];
+
             if (doorData.state === 'open') {
                 doorData.state = 'closed';
             }
+
+            broadcastRoomUpdatesForDoor(doorName);
+
             return {
                 name: doorName,
                 state: doorData.state
@@ -112,11 +137,13 @@ const resolvers = {
             const exitData = roomData.exits[exit];
             const doorName = exitData.door;
             const doorData = worldData.doors[doorName];
+
             if (doorData.state === 'closed') {
                 return {
                     reason: 'DOOR_CLOSED'
                 };
             }
+
             const targetRoomName = Object.keys(worldData.rooms)
                 .filter(roomName => room !== roomName)
                 .find(roomName => {
@@ -127,11 +154,14 @@ const resolvers = {
                         .includes(doorName);
                     return hasDoor;
                 });
+
             const targetRoom = worldData.rooms[targetRoomName];
+
             const roomExits = targetRoom.exits || {};
             const exits = Object.keys(roomExits)
                 .map((exitName) => {
                     const exit = roomExits[exitName];
+
                     return {
                         name: exitName,
                         description: exit.name,
@@ -142,6 +172,30 @@ const resolvers = {
                 name: targetRoomName,
                 description: targetRoom.description,
                 exits
+            }
+        },
+        speak: (_, { room, sessionId, message }) => {
+            pusher.trigger('presence-room-' + room, 'speak', {
+                user: users.getUser(sessionId),
+                message: message
+              }, sessionId);
+              return "Success";
+        },
+        shout: (_, { room, sessionId, message }) => {
+            pusher.trigger('global-events', 'shout', {
+                room: room,
+                user: users.getUser(sessionId),
+                message: message
+              }, sessionId);
+              return "Success";
+        }
+    },
+    ExitRoomResult: {
+        __resolveType: (obj) => {
+            if (obj.reason) {
+                return 'ExitRoomError';
+            } else {
+                return 'Room';
             }
         }
     }
