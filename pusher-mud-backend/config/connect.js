f9f55925de1673c464ad9519ac001bd719ec7f97
joshdelmonte <joshdelmonte@gmail.com>
2023-02-06 21:03:22 -0700

    Added .env

--- pusher-mud-backend/config/connect.js
+++ pusher-mud-backend/config/connect.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+//This will apply to the .env file
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bestiary_db', {
   useNewUrlParser: true,
