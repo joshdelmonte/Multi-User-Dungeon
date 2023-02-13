eb0c9d257062eb8d4234e2d3b25fdd5a43d2fffb
joshdelmonte <joshdelmonte@gmail.com>
2023-02-13 13:47:21 -0700

    Testing for heroku deployment

--- pusher-mud-backend/config/connect.js
+++ pusher-mud-backend/config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //This will apply to the .env file
-require('dotenv').config();
+// require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://Josh:mud123@cluster0.q00t495.mongodb.net:27017/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
